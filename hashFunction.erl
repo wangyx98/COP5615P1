9af89ce00f349544886313b912589c13d4008cf2
Xizhe Li <jasperqs7@gmail.com>
2022-09-13 21:15:35 -0400

    Add hash function

---
+++ hashFunction.erl
@@ -0,0 +1,9 @@
+-module(hashFunction).
+-export([encode/1]).
+
+hashit(String)->
+	<<Integer:256>> = crypto:hash(sha256, String),
+	Integer.
+encode(String)->
+	HexStr = string:right(integer_to_list(hashit(String),16),64,$0),
+	HexStr.
